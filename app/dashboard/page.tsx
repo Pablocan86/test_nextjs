83ad85e27995418f95d6dde9b4b0e2ac28ea4ab5
Pablocan86 <pablo.cantarin86@gmail.com>
2025-05-01 21:30:01 -0300

    primer subida

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function DashboardPage() {
+  return <p>Este es el contenido del dashboard</p>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Montserrat, Lusitana } from "next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export const lusitana = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
